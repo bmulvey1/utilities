5472fa3c4233d3a7b6293c203a08bae190f7f2f1
Bea Mulvey <22732775+bmulvey1@users.noreply.github.com>
2024-11-18 17:01:47 -0500

    set all monitors to correct position/resolution

---
+++ layout.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --mode 1920x1200 --pos 0x0 --rotate normal --output DP-2 --mode 1680x1050 --pos 3840x150 --rotate normal --output DP-3 --off --output DP-4 --primary --mode 1920x1080 --rate 143.86 --pos 1920x120 --rotate normal --output DP-5 --off --output USB-C-0 --off
